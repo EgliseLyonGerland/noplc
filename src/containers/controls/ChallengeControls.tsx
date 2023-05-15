9da473ad3bf5ed4342d881713354f04b2e20d03b
Nicolas Bazille <oltodo@msn.com>
2023-05-16 00:56:31 +0200

    Controls/Challenge: display lyrics

--- src/containers/controls/ChallengeControls.tsx
+++ src/containers/controls/ChallengeControls.tsx
@@ -1,7 +1,12 @@
+import clsx from "clsx";
+import { motion } from "framer-motion";
+
 import useAppState from "../../libs/useAppState";
 import useData from "../../libs/useData";
 import { getCurrentTeam } from "../../libs/utils";
 import { getChallenge } from "../../libs/utils";
+
+const lyricsLineHeight = 1.7;
 
 export default function ChallengeControls() {
   const { challenges } = useData();
@@ -14,7 +19,7 @@ export default function ChallengeControls() {
 
   const { challengeId, lyricsIndex, answer, status } = view;
 
-  const challenge = getChallenge(challenges, challengeId);
+  const { lyrics } = getChallenge(challenges, challengeId);
   const team = getCurrentTeam(state);
 
   const quit = () => {
@@ -24,18 +29,70 @@ export default function ChallengeControls() {
   const changeLyrics = (step: 1 | -1) => {
     dispatch({
       type: "challengeView.setLyricsIndex",
-      index: Math.max(
-        -1,
-        Math.min(lyricsIndex + step, challenge.lyrics.length - 1)
-      ),
+      index: Math.max(-1, Math.min(lyricsIndex + step, lyrics.length - 1)),
     });
   };
 
-  const isLast = lyricsIndex === challenge.lyrics.length - 1;
+  const isLast = lyricsIndex === lyrics.length - 1;
+
+  let startIndex: number;
+  let endIndex: number;
+
+  if (lyricsIndex < lyrics.length - 3) {
+    startIndex = Math.max(0, lyricsIndex - 3);
+    endIndex = Math.min(startIndex + 6, lyrics.length - 1);
+  } else {
+    endIndex = Math.min(lyricsIndex + 3, lyrics.length - 1);
+    startIndex = Math.max(0, endIndex - 6);
+  }
 
   return (
     <div>
-      <div className="flex gap-4">
+      <div className="mb-4 p-4 ring-1">
+        <div
+          className="relative overflow-hidden"
+          style={{ height: `${lyricsLineHeight * 7}em` }}
+        >
+          {lyrics.map((line, index) => {
+            if (index < startIndex - 1 || index > endIndex + 1) {
+              return;
+            }
+
+            return (
+              <motion.div
+                animate={
+                  index === startIndex - 1
+                    ? "previous"
+                    : index === endIndex + 1
+                    ? "next"
+                    : "shown"
+                }
+                className={clsx(
+                  "absolute uppercase transition-opacity",
+                  lyrics.length === index + 1 && "text-primary"
+                )}
+                initial={false}
+                key={index}
+                transition={{
+                  ease: "easeInOut",
+                }}
+                variants={{
+                  shown: {
+                    y: `${(index - startIndex) * lyricsLineHeight}em`,
+                    opacity: index === lyricsIndex ? 1 : 0.3,
+                  },
+                  previous: { y: `${-lyricsLineHeight}em`, opacity: 0 },
+                  next: { y: `${7 * lyricsLineHeight}em`, opacity: 0 },
+                }}
+              >
+                {line}
+              </motion.div>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="flex items-center gap-4">
         <button
           className="btn"
           disabled={lyricsIndex < 0}
@@ -50,6 +107,11 @@ export default function ChallengeControls() {
         >
           Paroles suivantes
         </button>
+        <span className="text-xl">
+          {lyricsIndex + 1}
+          {" / "}
+          {lyrics.length}
+        </span>
       </div>
       <div className="divider"></div>
       <form
